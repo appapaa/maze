8b7eee15672609a7de36fc9893d915f8145aa6b3
appapaa <appapaa@gmail.com>
2017-04-28 18:08:09 +0300

    2d

--- js/main.js
+++ js/main.js
@@ -5,8 +5,11 @@ var dTime = 50;
 var a = 0.00005;
 var dr = 0;
 var player = {
-	i: 0,
-	j: 0
+	i: 1,
+	j: 0,
+	new_i: 1,
+	new_i: 0,
+	check: 0
 };
 $( document ).ready(function() {
 	var $canv = $('canvas');
@@ -25,10 +28,10 @@ var pprint = function(a){
 //добавить круг
 var addArc = function(i, j, R){
     return {
-        x: Math.round(2*R*(i - (j%2)*Math.sin(Math.PI/6))),
-		y: Math.round(2*R*j*Math.cos(Math.PI/6)),
+        x: 2*R*i,
+		y: 2*R*j,
 		R: R,		
-		o: _.random(0, 5), // направление
+		o: _.random(0, 3), // направление
         draw: function(flg){
             ctx.beginPath();
             ctx.arc(this.x, this.y, this.R, 0, 2 * Math.PI, true);
@@ -37,9 +40,9 @@ var addArc = function(i, j, R){
 			ctx.strokeStyle = flg ? 'green' : '#EF4836';
 			ctx.lineWidth = 4;
 			ctx.translate(this.x, this.y);
-			ctx.rotate(-this.o*Math.PI/3);
+			ctx.rotate(-this.o*Math.PI/2);
 			ctx.beginPath();
-            ctx.arc(0, 0, this.R-3, Math.PI/6, -Math.PI/6, true);
+            ctx.arc(0, 0, this.R-2, Math.PI/4, -Math.PI/4, true);
             ctx.stroke();
 			ctx.restore();
 			if(player.i == i && player.j == j){
@@ -64,14 +67,11 @@ var addArc = function(i, j, R){
         },
 		direct: function(){
 			var o = this.o;
-			var d = ((player.i+1)%2);
 			var arr = {
 				0:{di: 1, dj: 0},
-				1:{di: d, dj: -1},
-				2:{di: -1 + d, dj: -1},
-				3:{di: -1, dj: 0},
-				4:{di: -1 + d, dj: 1},
-				5:{di: d, dj: 1},
+				1:{di: 0, dj: -1},
+				2:{di: -1, dj: 0},
+				3:{di: 0, dj: 1}
 				
 			}
 			return({i: arr[o].di + player.i, j: arr[o].dj + player.j});
@@ -89,12 +89,12 @@ var addArc = function(i, j, R){
 //массив кругов
 var arrArc = function(n){//n - чисчло кругов вдоль
     var arr = [];
-    var R = Math.ceil(w*0.48/(n + 1));
-	var m = Math.ceil(h*0.5/R);//число вругов ввысь
-    for(var i = 0; i < m; i++){
+    var R = Math.ceil(w*0.5/(n + 1));
+	var m = Math.ceil(h*0.5/R)-1;//число вругов ввысь
+    for(var i = 0; i < n; i++){
 		arr[i] = [];
-		for(var j = 0; j < n+(i%2); j++){
-			arr[i][j] = new addArc(j,i,R);
+		for(var j = 0; j < m; j++){
+			arr[i][j] = new addArc(i,j,R);
 		}
 	}
     return{
@@ -122,25 +122,38 @@ var arrArc = function(n){//n - чисчло кругов вдоль
 var go = function(){
 	ctx.fillStyle = 'white';
     ctx.fillRect(0,0,w,h);
-    var n = 10;
+    var n = 20;
     var arcs = arrArc(n);
 	
-	ctx.translate((w - (n-1)*2*arcs.R)/2,arcs.R*1.4);
+	ctx.translate(arcs.R*2,arcs.R*2);
     arcs.draw();
 	//drawPoint();
-    $( document ).on('click',function() {
-		var elem = arcs.elems[player.i][player.j]
-		elem.o++;
-		elem.o%=6;
-		var pos = elem.direct();
-		pprint(pos);
-		var check = 0;
-		if( pos.i >= 0 &&
-			pos.j >=0 &&
-			arcs.elems[pos.i]!=null &&
-			Math.abs(arcs.elems[pos.i][pos.j].o + elem.o) == 3){
-				check==1;
+	var point = {x: 0, y: 0};
+    $( document ).on('mousedown',function(e){
+	point = {x: e.pageX, y: e.pageY};
+	}).on('mouseup',function(e) {
+		var elem = arcs.elems[player.i][player.j];
+		if(Math.abs(e.pageX - point.x) < 10 && Math.abs(e.pageY - point.y) < 10){
+			elem.o++;
+			elem.o%=4;
+			var pos = elem.direct();
+			pprint(pos);
+			player.check = 0;
+			if( pos.i >= 0 &&
+				pos.j >=0 &&
+				arcs.elems[pos.i]!=null &&
+				Math.abs(arcs.elems[pos.i][pos.j].o - elem.o) == 2){
+					player.check = 1;
+					player.new_i = pos.i;
+					player.new_j = pos.j;
+			}
+			elem.clean().draw(player.check);
+		}else if(player.check) {
+			player.i = player.new_i;
+			player.j = player.new_j;
+			elem.clean().draw();
+			elem = arcs.elems[player.i][player.j];
+			elem.clean().draw();
 		}
-		elem.clean().draw(check);
-});
+	});
 };
